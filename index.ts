c6cc2ae3edc26f0c2a507dc96105a2b76d495172
林文雄 <U1000509>
2024-10-17 08:58:32 +0800

    -

--- index.ts
+++ index.ts
@@ -22,3 +22,35 @@ var proxySynchronousFile = (function(){
 for(let i=0; i<10; i++){
     proxySynchronousFile(i)
 }
+
+
+// ------------写成class形式
+class ProxySynchronousFile {
+    private cache: Number[]
+    private timer: any
+    constructor() {
+        this.cache = []
+    }
+    synchronousFile(id) {
+        console.log('开始同步发文件，id为：'+id)
+    }
+    setTimer(id) {
+        this.cache.push(id)
+        if(this.timer) {
+            return
+        }
+        this.timer = setTimeout(()=>{
+            this.synchronousFile(this.cache.join(','));
+            clearTimeout(this.timer)
+            this.timer = null
+            this.cache.length = 0
+        }, 2000)
+    }
+}
+
+var instance = new ProxySynchronousFile()
+for(let i = 0;i<10;i++) {
+    instance.setTimer(i)
+}
+
+
